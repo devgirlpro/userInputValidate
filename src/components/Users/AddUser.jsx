2459c05a8285de67b119b86a1c4baf3bb4e04a5a
Azadeh Galidari <agalidari@gmail.com>
2023-06-17 16:02:31 +0200

    fetching entered data from user and log it

--- src/components/Users/AddUser.jsx
+++ src/components/Users/AddUser.jsx
@@ -1,23 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./AddUser.module.css";
 import Card from "../UI/Card";
 import Button from "../UI/Button";
 
 const AddUser = () => {
+    const [enteredUsername, setEnteredUsername] = useState("");
+    const [enteredUserAge, setEnteredeUserAge] = useState("");
+
     const addUserHandler = (event) => {
-        event.preventDefoult();
+        event.preventDefault();
+        console.log(enteredUsername, enteredUserAge);
+    };
+
+    const usernameChangeHandler = (e) => {
+        setEnteredUsername(e.target.value);
+    };
+
+    const userAgeChangeHandler = (e) => {
+        setEnteredeUserAge(e.target.value);
     };
 
     return (
         <Card className={classes.input}>
             <form onSubmit={addUserHandler}>
-
                 <label htmlFor="username">Username</label>
-                <input id="username" type="text" />
-
+                <input
+                    id="username"
+                    type="text"
+                    onChange={usernameChangeHandler}
+                />
                 <label htmlFor="userage">Age (Year)</label>
-                <input id="userage" type="number" />
-
+                <input
+                    id="userage"
+                    type="number"
+                    onChange={userAgeChangeHandler}
+                />
                 <Button type="submit">Add User</Button>
                 <br /> <br />
                 <Button type="reset">Reset</Button>
