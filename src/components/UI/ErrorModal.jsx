fd0034ee4996f8cdc5ba0e4a5252a54305602c6a
Azadeh Galidari <agalidari@gmail.com>
2023-06-17 19:31:15 +0200

    confirm the error message box

---
+++ src/components/UI/ErrorModal.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Card from "./Card";
+import Button from "./Button";
+import calsses from "./ErrorModal.module.css";
+
+const ErrorModal = (props) => {
+    return (
+        <div>
+            <div className={calsses.backdrop} onClick={props.onConfirm}/>
+            <Card className={calsses.modal}>
+                <header className={calsses.header}>
+                    <h1>{props.title}</h1>
+                </header>
+                <div className={calsses.content}>
+                    <p>{props.message}</p>
+                </div>
+                <footer className={calsses.actions}>
+                    <Button onClick={props.onConfirm} >okay</Button>
+                </footer>
+            </Card>
+        </div>
+    );
+};
+
+export default ErrorModal;
--- src/components/Users/AddUser.jsx
+++ src/components/Users/AddUser.jsx
@@ -2,21 +2,31 @@ import React, { useState } from "react";
 import classes from "./AddUser.module.css";
 import Card from "../UI/Card";
 import Button from "../UI/Button";
+import ErrorModal from "../UI/ErrorModal";
 
 const AddUser = (props) => {
     const [enteredUsername, setEnteredUsername] = useState("");
     const [enteredUserAge, setEnteredeUserAge] = useState("");
-
+    const [error, setError] = useState();
     const addUserHandler = (event) => {
         event.preventDefault();
         if (
             enteredUsername.trim().length === 0 ||
             enteredUserAge.trim().length === 0
         ) {
+            setError({
+                title: "Invalid input",
+                message:
+                    "Please enter a valid name and age (non-empty values).",
+            });
             return;
         }
         //add + before enteredUserAge to change tring into number
         if (+enteredUserAge < 1) {
+            setError({
+                title: "Invalid age",
+                message: "Please input a valide age  (> 0)",
+            });
             return;
         }
 
@@ -34,28 +44,38 @@ const AddUser = (props) => {
         setEnteredeUserAge(e.target.value);
     };
 
+    const errorHandler = () => {
+      setError();
+    }
+
     return (
-        <Card className={classes.input}>
-            <form onSubmit={addUserHandler}>
-                <label htmlFor="username">Username</label>
-                <input
-                    id="username"
-                    type="text"
-                    value={enteredUsername}
-                    onChange={usernameChangeHandler}
-                />
-                <label htmlFor="userage">Age (Year)</label>
-                <input
-                    id="userage"
-                    type="number"
-                    value={enteredUserAge}
-                    onChange={userAgeChangeHandler}
-                />
-                <Button type="submit">Add User</Button>
-                <br /> <br />
-                <Button type="reset">Reset</Button>
-            </form>
-        </Card>
+        <div>
+            {error && (
+                <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler} />
+            )}
+
+            <Card className={classes.input}>
+                <form onSubmit={addUserHandler}>
+                    <label htmlFor="username">Username</label>
+                    <input
+                        id="username"
+                        type="text"
+                        value={enteredUsername}
+                        onChange={usernameChangeHandler}
+                    />
+                    <label htmlFor="userage">Age (Year)</label>
+                    <input
+                        id="userage"
+                        type="number"
+                        value={enteredUserAge}
+                        onChange={userAgeChangeHandler}
+                    />
+                    <Button type="submit">Add User</Button>
+                    <br /> <br />
+                    <Button type="reset">Reset</Button>
+                </form>
+            </Card>
+        </div>
     );
 };
 
