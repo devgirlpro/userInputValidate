0c3ee1eb8ef7d6fd607848841a666e510c1e8d0a
Azadeh Galidari <agalidari@gmail.com>
2023-06-19 04:54:03 +0200

    add Wrapper component

--- src/components/Users/AddUser.jsx
+++ src/components/Users/AddUser.jsx
@@ -3,6 +3,7 @@ import classes from "./AddUser.module.css";
 import Card from "../UI/Card";
 import Button from "../UI/Button";
 import ErrorModal from "../UI/ErrorModal";
+import Wrapper from "../Helpers/Wrapper";
 
 const AddUser = (props) => {
     const [enteredUsername, setEnteredUsername] = useState("");
@@ -49,7 +50,7 @@ const AddUser = (props) => {
     }
 
     return (
-        <div>
+        <Wrapper>
             {error && (
                 <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler} />
             )}
@@ -75,7 +76,7 @@ const AddUser = (props) => {
                     <Button type="reset">Reset</Button>
                 </form>
             </Card>
-        </div>
+        </Wrapper>
     );
 };
 
